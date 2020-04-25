e3f99b779fe4b7dd29d512e9575addfa0ff054b3
Naomi Akram <akram_naomi_s@student.smc.edu>
2020-04-24 17:19:51 -0700

    Initial Commit

---
+++ Assignment 3-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 3-2
+//
+//  Created by Naomi Akram on 4/24/20.
+//  Copyright © 2020 Naomi Akram. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
